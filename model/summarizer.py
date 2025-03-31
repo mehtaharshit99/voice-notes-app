22e64561bb94b3066ef5682e7002ed358e6ec66e
Harshit Mehta <harshitmehtadelhi@gmail.com>
2025-04-01 00:57:19 +0530

    Changes

--- model/summarizer.py
+++ model/summarizer.py
@@ -1,23 +1,7 @@
-from transformers import pipeline, AutoModelForSeq2SeqLM, AutoTokenizer
-import os
-
-MODEL_DIR = "./bart-large-cnn"
-
-def download_model():
-    """Downloads the model if not found."""
-    if not os.path.exists(MODEL_DIR):
-        print("Downloading model...")
-        model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
-        tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
-        
-        # Save model and tokenizer locally
-        model.save_pretrained(MODEL_DIR)
-        tokenizer.save_pretrained(MODEL_DIR)
+from transformers import pipeline
 
 def load_summarizer():
-    """Loads the summarization pipeline, ensuring the model is available."""
-    download_model()  # Ensure the model is downloaded before loading
-    return pipeline("summarization", model=MODEL_DIR, tokenizer=MODEL_DIR)
+    return pipeline("summarization", model="facebook/bart-base", tokenizer="facebook/bart-base")
 
 summarizer = load_summarizer()
 
