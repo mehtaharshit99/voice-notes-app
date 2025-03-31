4e750ad9d32270a45acdeaa22529f38a03f8216e
Harshit Mehta <harshitmehtadelhi@gmail.com>
2025-03-31 20:21:28 +0530

    changes to code

--- model/summarizer.py
+++ model/summarizer.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
-summarizer = pipeline("summarization", model="google/pegasus-xsum")
+summarizer = pipeline("summarization", model="t5-small")
 
 def summarize_text(text):
-    summary = summarizer(text, max_length=100, min_length=30, do_sample=False)
+    summary = summarizer("summarize: " + text, max_length=100, min_length=30, do_sample=False)
     return summary[0]["summary_text"]
