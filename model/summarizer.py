4f95e017bf7ff0c4acddf79b729564ffd812f59a
Harshit Mehta <harshitmehtadelhi@gmail.com>
2025-04-01 01:30:28 +0530

    changed model

--- model/summarizer.py
+++ model/summarizer.py
@@ -1,9 +1,9 @@
 from transformers import pipeline
 
 def load_summarizer():
-    return pipeline("summarization", model="facebook/bart-base", tokenizer="facebook/bart-base")
+    return pipeline("summarization", model="t5-small", tokenizer="t5-small")
 
 summarizer = load_summarizer()
 
 def summarize_text(text):
-    return summarizer(text, max_length=130, min_length=30, do_sample=False)[0]["summary_text"]
+    return summarizer(text, max_length=150, min_length=50, do_sample=False)[0]["summary_text"]
