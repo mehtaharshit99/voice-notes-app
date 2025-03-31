adb505bb08b4307cd4fa12f6260ee32cdf877593
Harshit Mehta <harshitmehtadelhi@gmail.com>
2025-03-31 19:44:04 +0530

    changes to code

--- model/summarizer.py
+++ model/summarizer.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
 
 # Specify the model for summarization explicitly
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn", use_fast=True)
+
 
 def summarize_text(text):
     summary = summarizer(text, max_length=150, min_length=50, do_sample=False)
