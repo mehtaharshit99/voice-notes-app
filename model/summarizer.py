5c15d163023bc1bed1bca2e59db770c1a82ba59a
Harshit Mehta <harshitmehtadelhi@gmail.com>
2025-04-01 00:04:27 +0530

    Removed large model file

--- model/summarizer.py
+++ model/summarizer.py
@@ -1,12 +1,9 @@
 from transformers import pipeline
-import streamlit as st
 
-@st.cache_resource()
 def load_summarizer():
-    return pipeline("summarization", model="./bart-large-cnn")
+    return pipeline("summarization", model="./bart-large-cnn", tokenizer="./bart-large-cnn")
 
 summarizer = load_summarizer()
 
 def summarize_text(text):
-    summary = summarizer(text, max_length=150, min_length=50, do_sample=False)
-    return summary[0]["summary_text"]
+    return summarizer(text, max_length=130, min_length=30, do_sample=False)[0]["summary_text"]
